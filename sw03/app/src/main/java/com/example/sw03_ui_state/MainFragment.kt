b7f25551083fe6e24cd422d332a256940b3d41b0
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2024-03-11 13:17:02 +0100

    wip

---
+++ sw03/app/src/main/java/com/example/sw03_ui_state/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.example.sw03_ui_state
+
+import android.os.Bundle
+import android.view.View
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
+
+
